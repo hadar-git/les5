f77e47f04bbd1afb2b895d113d9a1f5b8a67b18e
hadar <hadar01@kazoula.com>
2026-07-15 18:08:12 +0300

    add update and errors code

--- app.js
+++ app.js
@@ -6,24 +6,46 @@ const app = express()
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.status(200).send('Hello World')
 })
 
 app.get('/books', (req, res) => {
-  res.send(books);
+  res.status(200).send(books);
 })
 
 
 app.get('/books/:id', (req, res) => {
- res.send(books.find(book => Number(book.code) === Number(req.params.id)) );
+    let b=books.find(book => Number(book.code) === Number(req.params.id));
+    if(b)
+    {
+res.status(200).send(b );
+    }
+ else{
+    res.status(404).send("errorrrrrr");
+ }
 })
 
 
 app.post('/books/', (req, res) => {
-
-    
- res.send(books.push(req.body));
+    books.push(req.body)
+ res.status(201).send(req.body);
 })
+g
+
+app.put('/books/:id', (req, res) => {
+     const b= books.find(book => Number(book.code) === Number(req.params.id)) 
+if(b)
+{
+   b.name= req.body.name;
+    b.category=req.body.category;
+    b.price= req.body.price;
+ res.status(200).send(b);
+}
+else{
+     res.status(404).send("error");
+}
+
+});
 
 app.patch('/books/:id/borrow', (req, res) => {
     const b= books.find(book => Number(book.code) === Number(req.params.id)) 
@@ -31,10 +53,11 @@ app.patch('/books/:id/borrow', (req, res) => {
     if( b && b.isBorrowed ==false  )
     {
         b.isBorrowed=true;
-        res.send(b.loans.push({ borrowDate: new Date(), customerCode: req.body.customerCode }));
+        b.loans.push({ borrowDate: new Date(), customerCode: req.body.customerCode })
+        res.status(200).send(b);
     }
     else{
-        res.send('already in use');
+        res.status(404).send('already in use');
     }
 });
 
@@ -45,24 +68,25 @@ app.patch('/books/:id/return', (req, res) => {
     {
     console.log("returned");
     
-        res.send( b.isBorrowed=false);
+        res.status(200).send( b.isBorrowed=false);
     }
     else{
-        res.send('no idea');
+        res.status(404).send('no idea');
     }
 });
 
 app.delete('/books/:id', (req, res) => {
    const index = books.findIndex(book => Number(book.code) === Number(req.params.id));
 
+
     if(index!==-1)
    {
     console.log("ddd");
-    
-        res.send( books.splice(index, 1));
+  
+        res.status(200).send( books.splice(index, 1));
     }
     else{
-        res.send('not found');
+        res.status(404).send('not found');
     }
 });
 
